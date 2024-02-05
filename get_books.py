808d0a66b67818376c1f8813a32040e3d4b6354e
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-02-05 10:44:01 -0500

    Various small tweaks

--- get_books.py
+++ get_books.py
@@ -3,6 +3,8 @@ import requests
 def get_books():
     base_url = 'https://gutendex.com/books?languages=en'
     book_request = requests.get(base_url, params={'q': 'requests+lang:en'})
+    if book_request.status_code != 200:
+        raise Exception("Failed to fetch books from Gutendex API")
     books = book_request.json()['results']
     book_data = {}
     
@@ -10,7 +12,7 @@ def get_books():
         book_data[book['id']] = {
             'title': book['title'],
             'author': book['authors'][0]['name'].split()[0][:-1],
-            'url': book['formats']['text/plain; charset=us-ascii']
+            'url': book['formats'].get('text/plain; charset=us-ascii', 'Plaintext URL not available')
             }
 
     return book_data
